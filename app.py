1c8cba61f2824528ae0d4f6bb195478115181e22
ProffGarryOak <158749389+ProffGarryOak@users.noreply.github.com>
2024-02-05 13:23:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import numpy as np
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 
